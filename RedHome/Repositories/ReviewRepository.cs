89524675af18a8bcc6b51288a85933a7b51139ac
enjoyst1ck <bwwojcik0@gmail.com>
2024-04-10 12:37:56 +0200

    Created first repo

---
+++ RedHome/Repositories/ReviewRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RedHome.Database;
+using RedHome.Database.Models;
+
+namespace RedHome.Repositories
+{
+    public class ReviewRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public ReviewRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Review> GetReviews(string userId)
+        {
+            return _context.Reviews.Where(w => w.UserIdBy == userId).ToList();
+        }
+
+        public void Insert(Review review)
+        {
+            _context.Reviews.Add(review);
+        }
+
+        public void Edit(Review review)
+        {
+            _context.Entry(review).State = EntityState.Modified;
+        }
+
+        public void Delete(int reviewId)
+        {
+            Review? review = _context.Reviews.Find(reviewId);
+            if (review != null)
+            {
+                _context.Remove(review);
+            }
+        }
+    }
+}
