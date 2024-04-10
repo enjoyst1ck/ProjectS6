e74db0aae60704c0628856073254886512006b3e
enjoyst1ck <bwwojcik0@gmail.com>
2024-04-10 12:54:18 +0200

    Added first Dto - ReviewDto

---
+++ RedHome/Dtos/ReviewDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RedHome.Dtos
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string UserIdBy { get; set; } = string.Empty;
+        public string UserEmailBy { get; set; } = string.Empty;
+        public string UserIdTo { get; set; } = string.Empty;
+        public string UserEmailTo { get; set; } = string.Empty;
+        public int Rate { get; set; }
+        public string Comment { get; set; } = string.Empty;
+    }
+}
