f46e03fa07272d207214ac63b5b6747ed5a86af3
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-05-04 22:46:27 +0200

    Added responsiveness of menus and popular ones - can be improved

--- RedHome/ClientApp/src/component/PopularSearch.jsx
+++ RedHome/ClientApp/src/component/PopularSearch.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
 
 export default function PopularSearch() {
-	const popularSearch = [
-		{
-			content: "Studio apartment",
-			href: "#"
-		},
-		{
-			content: "Resident house",
-			href: "#"
-		},
-		{
-			content: "Countryside",
-			href: "#"
-		},
-		{
-			content: "Family house",
-			href: "#"
-		},
-	];
-	return (
-		<div className='p-3 w-[45%] m-auto'>
-			<div className='bg-white w-full h-full p-2 rounded-2xl flex justify-around items-center '>
-				<div className='p-1 px-2'>
-					<span className='text-lg font-bold'>Popular search:</span>
-				</div>
-				{popularSearch.map((item, index) => (
-					<div key={index} className=''>
-						<a href={item.href} className='p-2 px-6 bg-slate-800 text-white flex justify-center items-center rounded-xl text-lg'>{item.content}</a>
-					</div>
-				))}
-			</div>
-		</div>
-	)
+  const popularSearch = [
+    {
+      content: "Studio apartment",
+      href: "#"
+    },
+    {
+      content: "Resident house",
+      href: "#"
+    },
+    {
+      content: "Countryside",
+      href: "#"
+    },
+    {
+      content: "Family house",
+      href: "#"
+    },
+  ];
+  return (
+    <div className='p-3 2xl:w-[45%] xl:w-[53%] lg:w-[60%] 2xl:flex xl:flex lg:flex hidden m-auto'>
+      <div className='bg-white w-full h-full 2xl:p-2 p-1  rounded-2xl flex justify-around items-center flex-wrap'>
+        <div className='p-1 px-2'>
+          <span className='text-lg font-bold'>Popular search:</span>
+        </div>
+        {popularSearch.map((item, index) => (
+          <div key={index} className=''>
+            <a href={item.href} className='p-2 px-6 bg-slate-800 text-white flex justify-center items-center rounded-xl text-lg'>{item.content}</a>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
--- RedHome/ClientApp/src/component/SearchBarHome.jsx
+++ RedHome/ClientApp/src/component/SearchBarHome.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function SearchBarHome() {
 	return (
-		<div className='mt-10 p-3 w-[50%] m-auto'>
-			<div className='bg-white w-full h-full p-2 rounded-2xl flex '>
-				<div className='p-1 px-4 w-[25%] flex flex-col justify-center'>
+		<div className='lg:mt-10 mt-5 p-3 2xl:w-[55%] xl:w-[65%] lg:w-[78%] w-96 m-auto max-w-4xl'>
+			<div className='bg-white w-full h-full p-2 rounded-2xl flex flex-wrap'>
+				<div className='p-1 px-4 lg:w-[25%] w-96 flex flex-col justify-center'>
 					<select className='text-xl'>
 						<option className='text-lg'>Property type</option>
 						<option className='text-lg'>Property1</option>
@@ -13,7 +13,7 @@ export default function SearchBarHome() {
 					<p className='p-1 text-black text-opacity-75'>Select property type</p>
 				</div>
 
-				<div className='p-1 px-4 w-[25%] flex flex-col justify-center border border-y-0 border-black border-opacity-10'>
+				<div className='p-1 px-4 lg:w-[25%] w-96 flex flex-col justify-center lg:border lg:border-y-0 lg:border-black lg:border-opacity-10'>
 					<select className='text-xl'>
 						<option className='text-lg'>Location</option>
 						<option className='text-lg'>Property1</option>
@@ -22,7 +22,7 @@ export default function SearchBarHome() {
 					<p className='p-1 text-black text-opacity-75'>Select Location</p>
 				</div>
 
-				<div className='p-1 px-4 w-[25%] flex flex-col justify-center'>
+				<div className='p-1 px-4 lg:w-[25%] w-96 flex flex-col justify-center'>
 					<select className='text-xl'>
 						<option className='text-lg'>Offer type</option>
 						<option className='text-lg'>Property1</option>
@@ -31,7 +31,7 @@ export default function SearchBarHome() {
 					<p className='p-1 text-black text-opacity-75'>Select offer type</p>
 				</div>
 
-				<div className='p-1 px-4 w-[25%] flex flex-col justify-center'>
+				<div className='p-1 px-4 lg:w-[25%] w-96 flex flex-col justify-center'>
 					<button className='bg-slate-800 text-2xl p-2 flex justify-center items-center text-white rounded-2xl hover:scale-105 transition-all'>Search</button>
 				</div>
 			</div>
