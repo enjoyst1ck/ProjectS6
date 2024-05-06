b171beb3f28bf988bca9cdf34f265f02a0a24a8c
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-05-06 23:47:53 +0200

    added main info section

--- RedHome/ClientApp/src/pages/AdvertisingPage.jsx
+++ RedHome/ClientApp/src/pages/AdvertisingPage.jsx
@@ -18,8 +18,31 @@ export default function AdvertisingPage() {
           <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
         </div>
 
-        <div>
-          
+        <div className='grid grid-cols-5 mt-8'>
+          <div className='text-center flex flex-col'>
+            <span className='font-bold text-3xl'>102</span>
+            <span className='font-semibold text-xl text-black text-opacity-65'>Square m2</span> 
+          </div>
+
+          <div className='text-center flex flex-col'>
+            <span className='font-bold text-3xl'>3</span>
+            <span className='font-semibold text-xl text-black text-opacity-65'>Rooms</span> 
+          </div>
+
+          <div className='text-center flex flex-col'>
+            <span className='font-bold text-3xl'>1</span>
+            <span className='font-semibold text-xl text-black text-opacity-65'>Floor floor</span> 
+          </div>
+
+          <div className='text-center flex flex-col'>
+            <span className='font-bold text-3xl'>8</span>
+            <span className='font-semibold text-xl text-black text-opacity-65'>Which floor</span> 
+          </div>
+
+          <div className='text-center flex flex-col'>
+            <span className='font-bold text-3xl'>Block</span>
+            <span className='font-semibold text-xl text-black text-opacity-65'>Development type</span> 
+          </div>
         </div>
 
       </div>
