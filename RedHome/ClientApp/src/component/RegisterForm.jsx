14264c97b0376fea7184fb299859d685180301d0
miqueridamia <mwojtala0202@gmail.com>
2024-05-06 20:26:35 +0200

    added password match validation

--- RedHome/ClientApp/src/component/RegisterForm.jsx
+++ RedHome/ClientApp/src/component/RegisterForm.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { useForm } from 'react-hook-form';
 
 export default function RegisterForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, setError, watch, handleSubmit } = useForm({});
   const [data, setData] = useState("");
+  const password = useRef({});
+  password.current = watch("password", "");
+  
 
   console.log(window.innerWidth);
   return (
@@ -26,13 +29,15 @@ export default function RegisterForm() {
           <input className='w-full border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all mb-6'
             type="tel" {...register("phoneNumber", { required: true })} />
 
+          {/* Password match check */}
           <label className='text-xl font-semibold text-black text-opacity-50 '>Password:</label>
-          <input className='w-full border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all mb-6'
-            type='password' id='password' {...register("password", { required: true })} />
-
+          <input name='password' className='w-full border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all mb-6'
+            type='password' {...register("password", { required: true})} />
+             
           <label className='text-xl font-semibold text-black text-opacity-50 '>Repeat password:</label>
-          <input className='w-full border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'
-            type='password' id='repeatedpassword' {...register("repeatedPassword", { required: true })} />
+          <input name='passwordrepeat' className='w-full border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'
+            type='password' {...register("repeatedPassword", { required: true , validate: value => value === password.current})} />
+          {/* Password match check */}
           <a href='#' className='text-sm font-semibold text-black text-opacity-50'>Passwords must match!</a>
           <input type="submit" value='Sign up' className='mt-12 w-full bg-red-600 p-2 text-white text-xl font-bold rounded-xl hover:bg-red-700 transition-all cursor-pointer'/>
           <a href='#' className='text-sm font-semibold text-black text-opacity-50'>You already have an account? <span className='text-red-600 font-semibold text-opacity-75 underline'>Sign in</span></a>
