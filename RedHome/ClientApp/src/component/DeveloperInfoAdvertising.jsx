e7a625e3bf378b188d135781889e28ba5b077a85
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-05-07 14:41:43 +0200

    added main information and description of the property

---
+++ RedHome/ClientApp/src/component/DeveloperInfoAdvertising.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function DeveloperInfoAdvertising() {
+  return (
+    <div className='w-[25%]'>
+      <div className='p-4 w-full h-full border-black border-opacity-50 border-2 rounded-2xl'>
+        <div className='flex items-center'>
+          <div className='size-12 bg-red-100 rounded-full'>
+            <img src='' />
+          </div>
+          <span className='text-xl font-bold ml-3'>Andrzej Kubica</span>
+        </div>
+
+        <div className='my-12 flex flex-col'>
+          <span className='text-xl font-bold'>Contact details</span>
+          <div className='flex flex-col mt-2'>
+            <span>E-mail:</span>
+            <span>adnrzejkubica@gmail.com</span>
+          </div>
+          <div className='flex flex-col mt-2'>
+            <span>Phone number:</span>
+            <span>+48 123 456 789</span>
+          </div>
+          <div className='flex flex-col mt-2'>
+            <span>Other information:</span>
+            <span>Not available until June 15, 2024</span>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    </div>
+  )
+}
--- RedHome/ClientApp/src/pages/AdvertisingPage.jsx
+++ RedHome/ClientApp/src/pages/AdvertisingPage.jsx
@@ -1,50 +1,21 @@
 import React from 'react'
+import DeveloperInfoAdvertising from '../component/DeveloperInfoAdvertising'
+import ContentAdvertising from '../component/ContentAdvertising'
+import InfoAdvertising from '../component/InfoAdvertising'
+import SliderAdvertising from '../component/SliderAdvertising'
 
 export default function AdvertisingPage() {
   return (
-    <div className='w-[75%] mx-auto p-2 test'>
+    <div className='w-[75%] mx-auto p-2'>
       <div className='w-full'>
-        <div className='w-full h-[723px] bg-black rounded-2xl relative'>
-          <div className='bg-white rounded-full size-12 absolute right-4 top-4'>
+        <SliderAdvertising/>
 
-          </div>
+        <InfoAdvertising/>
+
+        <div className='flex cursor-default mb-10'>
+          <ContentAdvertising/>
+          <DeveloperInfoAdvertising/>
         </div>
-        <div className='w-full grid grid-cols-6 mt-3 gap-3'>
-          <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
-          <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
-          <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
-          <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
-          <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
-          <div className='w-full bg-black aspect-[4/2] rounded-2xl'></div>
-        </div>
-
-        <div className='grid grid-cols-5 mt-8'>
-          <div className='text-center flex flex-col'>
-            <span className='font-bold text-3xl'>102</span>
-            <span className='font-semibold text-xl text-black text-opacity-65'>Square m2</span> 
-          </div>
-
-          <div className='text-center flex flex-col'>
-            <span className='font-bold text-3xl'>3</span>
-            <span className='font-semibold text-xl text-black text-opacity-65'>Rooms</span> 
-          </div>
-
-          <div className='text-center flex flex-col'>
-            <span className='font-bold text-3xl'>1</span>
-            <span className='font-semibold text-xl text-black text-opacity-65'>Floor floor</span> 
-          </div>
-
-          <div className='text-center flex flex-col'>
-            <span className='font-bold text-3xl'>8</span>
-            <span className='font-semibold text-xl text-black text-opacity-65'>Which floor</span> 
-          </div>
-
-          <div className='text-center flex flex-col'>
-            <span className='font-bold text-3xl'>Block</span>
-            <span className='font-semibold text-xl text-black text-opacity-65'>Development type</span> 
-          </div>
-        </div>
-
       </div>
     </div>
   )
