b84cab3659a87a37202b567d357d2487c7bdf484
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-05-07 15:05:11 +0200

    add simple fetch advertisment

--- RedHome/ClientApp/src/App.jsx
+++ RedHome/ClientApp/src/App.jsx
@@ -1,14 +1,6 @@
 import AdvertisingPage from "./pages/AdvertisingPage";
 
-function App() {
-  async function logAdvertisement() {
-    const response = await fetch("http://localhost:5242/Advertisement");
-    const advertisement = await response.json();
-    console.table(advertisement[1].city);
-  }
-
-  logAdvertisement();
-  
+function App() {  
   return (
     <>
       <AdvertisingPage/>
--- RedHome/ClientApp/src/pages/AdvertisingPage.jsx
+++ RedHome/ClientApp/src/pages/AdvertisingPage.jsx
@@ -1,20 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DeveloperInfoAdvertising from '../component/DeveloperInfoAdvertising'
 import ContentAdvertising from '../component/ContentAdvertising'
 import InfoAdvertising from '../component/InfoAdvertising'
 import SliderAdvertising from '../component/SliderAdvertising'
 
 export default function AdvertisingPage() {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+
+    fetch("http://localhost:5242/Advertisement")
+      .then(res => {
+        if (!res.ok) throw new Error("Network response was not ok");
+        return res.json();
+      })
+      .then(data => {
+        setData(data[1]);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        setIsPending(false);
+        setError(err.message);
+      });
+
+  }, []);
+
+  console.table(data)
+
   return (
     <div className='w-[75%] mx-auto p-2'>
       <div className='w-full'>
-        <SliderAdvertising/>
+        <SliderAdvertising />
 
-        <InfoAdvertising/>
+        <InfoAdvertising />
 
         <div className='flex cursor-default mb-10'>
-          <ContentAdvertising/>
-          <DeveloperInfoAdvertising/>
+          <ContentAdvertising />
+          <DeveloperInfoAdvertising />
         </div>
       </div>
     </div>
