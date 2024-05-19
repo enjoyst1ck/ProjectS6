6e82b77138818e22b72251b00a3ff5ab3ea00627
Kamil <134055@student.uni.opole.pl>
2024-05-19 21:12:53 +0200

    Changed area variable type in model and added migration

---
+++ RedHome/Migrations/20240519190857_ChangeAreaVarType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RedHome.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAreaVarType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Area",
+                table: "Advertisements",
+                type: "decimal(18,2)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Area",
+                table: "Advertisements",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldPrecision: 18,
+                oldScale: 2)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
