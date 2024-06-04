40b1d335140f2a3b9f36588b65cff36c2390f36b
miqueridamia <mwojtala0202@gmail.com>
2024-06-05 00:49:39 +0200

    View of user settings + dependencies

--- RedHome/ClientApp/src/pages/UserAdvertisingPage.jsx
+++ RedHome/ClientApp/src/pages/UserAdvertisingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import UserPanel from '../component/UserPanel'
+import UserSettings from '../component/UserSettings'
 
 export default function UserAdvertisingPage() {
   return (
     <div className='w-[75%] mx-auto'>
       <UserPanel/>
+      <div >
+        <UserSettings></UserSettings>
+      </div>
     </div>
   )
 }
