c7a541a8a6a6474be44b00cfbb6d7b31000d150a
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-06-05 19:48:46 +0200

    Change design site

--- RedHome/ClientApp/src/component/NavAddPage.jsx
+++ RedHome/ClientApp/src/component/NavAddPage.jsx
@@ -14,12 +14,16 @@ export default function NavAddPage({ currStep, setCurrStep }) {
          setCurrStep(prev => prev - 1);
       }
    };
+
+   const jumpBetwennCard = (curr) => {
+      console.log('44456654');
+   };
    return (
       <div className='w-full flex justify-between items-center'>
          <div className='py-1 px-6 bg-[#BE3144] text-white text-center text-xl rounded-xl cursor-pointer hover:bg-[#be3144ec] transition-all' onClick={prevHandle}>Previus</div>
          <div className='flex '>
             {helper.map((item, index) => (
-               <div className={index === currStep - 1 ? 'size-6 rounded-full border-4 bg-[#be3144ec] border-[#be3144ec] mx-1' : 'size-6 rounded-full border-4 border-[#be3144ec] mx-1'}></div>
+               <div key={index} onClick={() => jumpBetwennCard(index)}  className={index === currStep - 1 ? 'size-6 rounded-full border-4 bg-[#be3144ec] border-[#be3144ec] mx-1' : 'size-6 rounded-full border-4 border-[#be3144ec] mx-1'}></div>
             ))}
          </div>
          <div className='py-1 px-6 bg-[#BE3144] text-white text-center text-xl rounded-xl cursor-pointer hover:bg-[#be3144ec] transition-all' onClick={nextHandle}>Next</div>
--- RedHome/ClientApp/src/pages/AddPage.jsx
+++ RedHome/ClientApp/src/pages/AddPage.jsx
@@ -8,7 +8,7 @@ import NavAddPage from '../component/NavAddPage';
 export default function AddPage() {
    const [currStep, setCurrStep] = useState(1)
    return (
-      <div className='w-[75%] mx-auto flex flex-col'>
+      <div className='w-[75%] my-28 mx-auto flex flex-col'>
          <div className='w-full flex flex-col'>
             <div className='flex flex-col'>
                <span className='text-black text-opacity-70 text-lg'>Step {currStep}</span>
@@ -16,14 +16,14 @@ export default function AddPage() {
             </div>
             <div className='flex mt-8'>
                <div className='mr-5 w-64 h-40 items-center flex justify-center'>
-                  <input type="radio" id="isSell-true" name="isSell" value="true" class="hidden peer" />
+                  <input type="radio" id="isSell-true" name="isForSell" value="true" class="hidden peer" />
                   <label for="isSell-true" class="flex justify-center flex-col w-full h-full border-[3px] border-[#FFD7DC] cursor-pointer peer-checked:border-[#BE3144] peer-checked:text-[#BE3144] hover:text-gray-600 transition-all rounded-2xl">
                      <div className='flex justify-center'><MdOutlineSell size={48} /></div>
                      <div className='text-center text-2xl mt-3'>Sell</div>
                   </label>
                </div>
                <div className='mr-5 w-64 h-40 items-center flex justify-center'>
-                  <input type="radio" id="isSell-false" name="isSell" value="false" class="hidden peer" />
+                  <input type="radio" id="isSell-false" name="isForSell" value="false" class="hidden peer" />
                   <label for="isSell-false" class="flex justify-center flex-col w-full h-full border-[3px] border-[#FFD7DC] cursor-pointer peer-checked:border-[#BE3144] peer-checked:text-[#BE3144] hover:text-gray-600 transition-all rounded-2xl">
                      <div className='flex justify-center'><CiCalendar size={48} /></div>
                      <div className='text-center text-2xl mt-3'>Rent</div>
@@ -49,9 +49,64 @@ export default function AddPage() {
                   </label>
                </div>
             </div>
-
          </div>
-         <NavAddPage currStep={currStep} setCurrStep={setCurrStep} />
+
+         <div className='flex flex-col'>
+            <div className='font-bold text-2xl mt-16'>Basic information</div>
+            <label className='mt-5 text-lg font-semibold'>Advertisement title:</label>
+            <input type="text" className='mt-2 w-[50%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all' />
+
+            <div className='grid grid-cols-3 mt-10 gap-5'>
+               <div className='flex justify-center flex-col w-[80%]'>
+                  <label className='text-lg font-semibold'>Price</label>
+                  <div className='w-full'>
+                     <input type='text' className='mt-2 w-[75%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'/>
+                  </div>
+               </div>
+               <div className='flex justify-center flex-col w-[80%]'>
+                  <label className='text-lg font-semibold'>Square m2</label>
+                  <div className='w-full'>
+                     <input type='text' className='mt-2 w-[75%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'/>
+                  </div>
+               </div>
+               <div className='flex justify-center flex-col w-[80%]'>
+                  <label className='text-lg font-semibold'>Price</label>
+                  <div className='w-full'>
+                     <input type='text' className='mt-2 w-[75%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'/>
+                  </div>
+               </div>
+
+               <div className='flex justify-center flex-col w-[80%]'>
+                  <label className='text-lg font-semibold'>Rooms quantity</label>
+                  <div className='w-full'>
+                     <select className='mt-2 w-[75%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'>
+
+                     </select>
+                  </div>
+               </div>
+               <div className='flex justify-center flex-col w-[80%]'>
+                  <label className='text-lg font-semibold'>Square m2</label>
+                  <div className='w-full'>
+                     <input type='text' className='mt-2 w-[75%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'/>
+                  </div>
+               </div>
+               <div className='flex justify-center flex-col w-[80%]'>
+                  <label className='text-lg font-semibold'>Price</label>
+                  <div className='w-full'>
+                     <input type='text' className='mt-2 w-[75%] p-2 border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all'/>
+                  </div>
+               </div>
+            </div>
+         </div>
+
+         <div className='flex flex-col'>
+            <div className='font-bold text-2xl mt-16'>Detail information</div>
+            <label className='mt-5 text-lg'>Advertisement description:</label>
+            <textarea className='mt-2 w-[50%] min-h-96 p-2 border-red-600 border-2 py-1 px-2 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all' />
+         </div>
+
+
+         {/* <NavAddPage currStep={currStep} setCurrStep={setCurrStep} /> */}
       </div>
    )
 }
