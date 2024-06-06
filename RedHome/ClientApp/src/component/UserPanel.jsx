27e3577ab80434a295801aae504ab63270cb1579
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-06-06 16:42:40 +0200

    The display of the likes and added lists has been changed, and display components have been created

--- RedHome/ClientApp/src/component/UserPanel.jsx
+++ RedHome/ClientApp/src/component/UserPanel.jsx
@@ -31,64 +31,10 @@ export default function UserPanel() {
     },
   ];
 
-  const [data, setData] = useState([]);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch(`http://localhost:7004/Advertisement/getByUser/32387598-4ada-4dea-9e86-0921eadce60a`)
-      .then((res) => {
-        if (!res.ok) throw Error("Could not fetch");
-        return res.json();
-      })
-      .then((data) => {
-        setData(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setIsPending(false);
-        setError(err);
-      });
-  }, []);
-
-  console.log(data)
-
   return (
     <div className='w-full mt-20'>
       <div className='grid grid-cols-4 p-5 gap-2'>
         {userTile.map((item, index) => (<UserPanelTile userTile={item} key={index} />))}
-      </div>
-
-      <div className='mt-10 w-full p-5'>
-        <div className=''>
-          <span className='text-2xl'>All advertisement {data.length}:</span>
-        </div>
-        <div className='w-full min-h-36 '>
-          {data.length === 0 && <span className='w-full mt-20 text-xl font-semibold flex justify-center'>You don't have any advertisement yet</span>}
-
-          {data.map(item => (
-            <div className='w-full h-16 rounded-2xl border-[2px] border-[#ffdbdf] hover:border-[#BE3144] transition-all duration-500 mt-2 grid grid-cols-12'>
-              <div className='w-full h-full col-span-3 rounded-2xl flex items-center p-4 font-semibold'>
-                {item.title}
-              </div>
-              <div className='w-full h-full col-span-2 rounded-2xl flex items-center p-4 font-semibold'>
-                {item.isForSell === true ? "For sale" : "For rent"}
-              </div>
-              <div className='w-full h-full col-span-2 rounded-2xl flex items-center p-4 font-semibold'>
-                {item.price + " zł"}
-              </div>
-              <div className='w-full h-full col-span-2 rounded-2xl flex items-center p-4 font-semibold'>
-                {item.attachments.length + " photos"}
-              </div>
-              <div className='w-full h-full col-span-3 rounded-2xl flex items-center p-3 font-semibold'>
-                <button className='py-1 px-8 mr-1 rounded-lg border-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white transition-all'>View</button>
-                <button className='py-1 px-8 mr-1 rounded-lg border-2 border-yellow-600 text-yellow-600 hover:bg-yellow-600 hover:text-white transition-all'>Edit</button>
-                <button className='py-1 px-8 mr-1 rounded-lg border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all'>Delete</button>
-              </div>
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   )
--- RedHome/ClientApp/src/pages/UserAdvertisingPage.jsx
+++ RedHome/ClientApp/src/pages/UserAdvertisingPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UserPanel from '../component/UserPanel'
+import ListUserAdvertisng from '../component/ListUserAdvertisng'
 
 export default function UserAdvertisingPage() {
   return (
     <div className='w-[75%] mx-auto'>
       <UserPanel/>
+      <ListUserAdvertisng/>
     </div>
   )
 }
