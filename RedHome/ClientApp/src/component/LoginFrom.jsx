57779e62536ec7ea5590bed5fa246daf7ab95f86
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-06-09 19:52:44 +0200

    connected login page with backend

--- RedHome/ClientApp/src/component/LoginFrom.jsx
+++ RedHome/ClientApp/src/component/LoginFrom.jsx
@@ -7,6 +7,7 @@ export default function LoginFrom() {
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState("");
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
@@ -15,13 +16,22 @@ export default function LoginFrom() {
     }
   }, [navigate]);
 
+  const onSubmit = async (formData) => {
+    try {
+      await login(formData);
+      navigate('/');
+    } catch (error) {
+      console.error('Registration error:', error.response ? error.response.data : error.message);
+    }
+  };
+
   return (
     <div className='w-[100%] xl:w-[40%] lg:w-[50%] sm:w-[100%] z-10 h-screen flex justify-center items-center'>
       <div className='w-[95%] xl:w-[65%] lg:w-[65%] sm:w-[65%] xl:p-0 lg:p-0 sm:p-5 p-5 bg-white rounded-2xl'>
         <h1 className='text-4xl font-bold'>Log <span className='text-red-600'>in</span></h1>
         <p className='text-xl font-semibold mt-8 text-black text-opacity-50'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dapibus non magna in imperdiet. </p>
 
-        <form className='mt-10' onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+        <form className='mt-10' onSubmit={handleSubmit(onSubmit)}>
           <label className='text-xl font-semibold text-black text-opacity-50'>E-mail:</label>
           <input className='w-full border-red-600 border-2 py-1 px-2 h-10 rounded-xl border-opacity-50 focus:border-opacity-100 outline-none transition-all mb-6'
             type="email" {...register("email", { required: true })} />
