547d48d14395b0588ec1cf51ee659a717fc754a8
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-06-10 19:04:49 +0200

    added custome react hook

---
+++ RedHome/ClientApp/src/hooks/useFetch.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) throw Error("Could not fetch");
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
+      });
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
+export default useFetch;
