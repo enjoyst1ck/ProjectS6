845905ea3a39fb3c18322fee230ae6c895980ab3
miqueridamia <mwojtala0202@gmail.com>
2024-06-13 16:42:35 +0200

    display foreign user

--- RedHome/ClientApp/src/pages/SearchPage.jsx
+++ RedHome/ClientApp/src/pages/SearchPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TopSearchBar from './../component/TopSearchBar'
 import SearchSection from './../component/SearchSection'
+import ForeignUserRating from '../component/ForeingUserRating';
 
 export default function SearchPage() {
   return (
     <div>
       <TopSearchBar/>
+      {/* usunac foreign user*/}
+      <ForeignUserRating></ForeignUserRating>
       <SearchSection/>
     </div>
   )
