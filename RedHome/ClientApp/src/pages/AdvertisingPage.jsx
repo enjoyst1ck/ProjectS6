400d1aa16c847822c373f759ff349fed2223a5a3
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-06-13 16:04:40 +0200

    added add to fav in advertisment page

--- RedHome/ClientApp/src/pages/AdvertisingPage.jsx
+++ RedHome/ClientApp/src/pages/AdvertisingPage.jsx
@@ -1,19 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import DeveloperInfoAdvertising from '../component/DeveloperInfoAdvertising'
 import ContentAdvertising from '../component/ContentAdvertising'
 import InfoAdvertising from '../component/InfoAdvertising'
 import SliderAdvertising from '../component/SliderAdvertising'
 import { Link, useParams } from 'react-router-dom'
 import TopBar from '../component/TopBar'
+import TopMenu from '../component/TopMenu'
+import { AuthContext } from '../context/authContext'
 
 export default function AdvertisingPage() {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
+  const [isLiked, setIsLiked] = useState(null);
+  const {currentUser} = useContext(AuthContext);
 
   useEffect(() => {
-    fetch(`http://localhost:7004/Advertisement/${id}`)
+    fetch(`http://localhost:7004/Advertisement/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${currentUser && currentUser.token}`
+      }
+    })
       .then((res) => {
         if (!res.ok) {
           throw new Error('Could not fetch the data for that resource');
@@ -22,6 +30,7 @@ export default function AdvertisingPage() {
       })
       .then((data) => {
         setData(data);
+        setIsLiked(data.isLiked);
         setIsPending(false);
         setError(null);
       })
@@ -31,19 +40,22 @@ export default function AdvertisingPage() {
       });
   }, [id]);
 
+  console.log(data)
 
   return (
     <div>
-      <TopBar/>
+      <div className='bg-white drop-shadow-xl pb-4'>
+        <TopMenu />
+      </div>
       <div className='w-[75%] mx-auto p-2 m-10'>
-        
+
         {data !== null ? (<div className='w-full'>
-          <SliderAdvertising images={data.attachments} isLiked={data.isLiked}/>
+          <SliderAdvertising images={data.attachments} isLiked={isLiked} setIsLiked={setIsLiked} id={data.id} />
 
           <InfoAdvertising area={data.area} rooms={data.roomQuantity} floor={data.floor} developmentType={data.developmentType} floorQuantity={data.floorQuantity} />
 
           <div className='flex cursor-default mb-10'>
-            <ContentAdvertising title={data.title} description={data.description} price={data.price} isForSell={data.isForSell} city={data.city} address={data.address}/>
+            <ContentAdvertising title={data.title} description={data.description} price={data.price} isForSell={data.isForSell} city={data.city} address={data.address} />
             <DeveloperInfoAdvertising />
           </div>
         </div>) : (<div className='w-full h-[90vh]'>
@@ -55,11 +67,11 @@ export default function AdvertisingPage() {
               <p className='text-4xl'>The advertised ad apparently doesn't exist</p>
               <Link to='/search' className='text-3xl mt-10 border-4 px-9 py-3 border-red-600 rounded-3xl text-red-600 font-semibold hover:bg-red-600 hover:text-white transition-all'>Back to search</Link>
             </div>
-            
-            
+
+
           </div>
         </div>)}
-    </div>
+      </div>
     </div>
   )
 }
