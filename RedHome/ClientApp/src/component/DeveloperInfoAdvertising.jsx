a83fce81725745d9150c33267f871664f269e9b7
Kacper Mielańczyk <kacpermielanczyk@gmail.com>
2024-06-17 15:36:18 +0200

    fixed whole page

--- RedHome/ClientApp/src/component/DeveloperInfoAdvertising.jsx
+++ RedHome/ClientApp/src/component/DeveloperInfoAdvertising.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
+import React from 'react';
+import avatar from '../assets/avatar.jpg';
 
-export default function DeveloperInfoAdvertising({ userEmail }) {
+export default function DeveloperInfoAdvertising({ username, phoneNumber, userEmail }) {
   return (
     <div className='w-[25%]'>
-      <div className='p-4 w-full h-full border-black border-opacity-50 border-2 rounded-2xl'>
+      <div className='p-4 w-full h-96 border-black border-opacity-50 border-2 rounded-2xl'>
         <div className='flex items-center'>
           <div className='size-12 bg-red-100 rounded-full'>
-            <img src='' />
+            <img src={avatar} />
           </div>
-          <span className='text-xl font-bold ml-3'>Andrzej Kubica</span>
+          <span className='text-xl font-bold ml-3'>{username || userEmail}</span>
         </div>
 
         <div className='my-12 flex flex-col'>
           <span className='text-xl font-bold'>Contact details</span>
-          <div className='flex flex-col mt-2'>
-            <span>E-mail:</span>
-            <span>adnrzejkubica@gmail.com</span>
-          </div>
-          <div className='flex flex-col mt-2'>
-            <span>Phone number:</span>
-            <span>+48 123 456 789</span>
-          </div>
-          <div className='flex flex-col mt-2'>
-            <span>Other information:</span>
-            <span>Not available until June 15, 2024</span>
+
+          <div className='h-56 flex flex-col'>
+            <div className='flex flex-col mt-2'>
+              <span>E-mail:</span>
+              <span className='underline'><a href={`mailto:${userEmail}`}>{userEmail}</a></span>
+            </div>
+            <div className='flex flex-col mt-8'>
+              <span>Phone number:</span>
+              <span>{phoneNumber.length > 0 ? <a className='underline' href={`tel:${phoneNumber}`}>{phoneNumber}</a> : 'None'}</span>
+            </div>
+            <div className='flex flex-col mt-8'>
+              <span>Other information:</span>
+              <span>None</span>
+            </div>
           </div>
         </div>
 
